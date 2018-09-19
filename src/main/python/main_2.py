a5dfb1151a4208859d3517740280620de34186db
Michael Herrmann <michael@herrmann.io>
2018-09-19 13:23:34 +0200

    Add file forgotten in previous commit

--- src/main/python/main_2.py
+++ src/main/python/main_2.py
@@ -1,24 +1,36 @@
-from fbs_runtime.application_context import ApplicationContext, \
-    cached_property
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QMainWindow, QLabel
+from fbs_runtime.application_context import ApplicationContext, cached_property
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout
 
+import requests
 import sys
 
 class AppContext(ApplicationContext):
     def run(self):
-        self.main_window.show()
+        stylesheet = self.get_resource('styles.qss')
+        self.app.setStyleSheet(open(stylesheet).read())
+        self.window.show()
         return self.app.exec_()
     @cached_property
-    def main_window(self):
-        result = QMainWindow()
-        image_container = QLabel()
-        image_container.setPixmap(self.image)
-        result.setCentralWidget(image_container)
-        return result
-    @cached_property
-    def image(self):
-        return QPixmap(self.get_resource('success.jpg'))
+    def window(self):
+        return MainWindow()
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        text = QLabel()
+        text.setWordWrap(True)
+        button = QPushButton('Next quote >')
+        button.clicked.connect(lambda: text.setText(_get_quote()))
+        layout = QVBoxLayout()
+        layout.addWidget(text)
+        layout.addWidget(button)
+        layout.setAlignment(button, Qt.AlignHCenter)
+        self.setLayout(layout)
+
+def _get_quote():
+    response = requests.get('https://talaikis.com/api/quotes/random/')
+    return response.json()['quote']
 
 if __name__ == '__main__':
     appctxt = AppContext()
